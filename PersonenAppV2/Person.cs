70f3a7f07566c275d9ec02c3bb84f4070a703441
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2020-09-04 09:33:00 +0200

    Schritt 3

--- PersonenAppV2/Person.cs
+++ PersonenAppV2/Person.cs
@@ -6,14 +6,31 @@ using System.Threading.Tasks;
 
 namespace PersonenAppV2 {
     class Person {
-        protected int mPersNr;
-        protected string mAnrede;
-        protected string mName;
-        protected string mVorname;
-        protected string mPlz;
-        protected string mOrt;
-        protected int mEintrittsjahr;
-        protected double mSalaer;
-        protected double mPensum;
+        private const int minEintrittsjahr = 1975;
+        private const double maxSalaer = 99999.95;
+        protected int mPersNr { get; }
+        protected string mAnrede { get; set; }
+        protected string mName { get; set; }
+        protected string mVorname { get; set; }
+        protected string mPlz { get; set; }
+        protected string mOrt { get; set; }
+        protected int mEintrittsjahr {
+            get { return mEintrittsjahr; }
+            set {
+                mEintrittsjahr = value;
+                if (value < minEintrittsjahr) { mEintrittsjahr = minEintrittsjahr; }
+                if (value > 2020) { mEintrittsjahr = 2020; }
+            }
+        }
+        protected double mSalaer {
+            get { return mSalaer; }
+            set {
+                mSalaer = value;
+                if (value < 0) { mSalaer = 0; }
+                if (value > maxSalaer) { mSalaer = maxSalaer; }
+            }
+        }
+        protected double mPensum { get; set; }
     }
+
 }
