7180e41c5ecc0ddbc540760d7d910a31bc69d26c
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2020-09-04 09:16:14 +0200

    Schritt 2
    Completed Step 2

---
+++ PersonenAppV2/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonenAppV2 {
+    class Person {
+        protected int mPersNr;
+        protected string mAnrede;
+        protected string mName;
+        protected string mVorname;
+        protected string mPlz;
+        protected string mOrt;
+        protected int mEintrittsjahr;
+        protected double mSalaer;
+        protected double mPensum;
+    }
+}
